54bcb0de6152a6e1a0e65284bd4ecd48316b2f80
PXgamer <owzie123@gmail.com>
2016-05-07 23:07:04 +0100

    Added the first library
    Added a library for getting blocked users as an array.

---
+++ Libraries/Get Blocked Users.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Get Blocked Users
+// @namespace    PXgamer
+// @version      0.3
+// @description  Get a list of your blocked users.
+// @author       PXgamer
+// @match        *kat.cr/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var returnedData;
+    var searchString = /<li id\=\"blocked\_.+\"\><span class\=\"badgeInline\"\><span class\=\"offline\" title\=\"offline\"\><\/span\> <span class\=\"aclColor_[0-9]+\"\><a class\=\"plain\" href\=\"\/user\/.+\/\"\>([a-z0-9]+)<\/a>/gi, matches, blockedUsers = [];
+
+    $.ajax({
+        type: "GET",
+        url: "/settings/privacy/",
+        success: function (data) {
+            returnedData = data;
+            while (matches = searchString.exec(returnedData.html)) { // FUCK THIS ERROR, it doesn't matter. :P
+                blockedUsers.push(matches[1]);
+            }
+            console.log(blockedUsers);
+        },
+        returnData: "json"
+    });
+})();
