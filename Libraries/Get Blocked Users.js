c1a8ff9149bdcf84feb43416cc61905cf8eb178c
PXgamer <owzie123@gmail.com>
2016-05-08 12:53:08 +0100

    Fixed regex for some names.

--- Libraries/Get Blocked Users.js
+++ Libraries/Get Blocked Users.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Get Blocked Users
 // @namespace    PXgamer
-// @version      0.4
+// @version      0.5
 // @description  Get a list of your blocked users.
 // @author       PXgamer
 // @match        *kat.cr/*
@@ -12,20 +12,19 @@ function gbu() {
     'use strict';
 
     var returnedData;
-    var searchString = /<li id\=\"blocked\_.+\"\><span class\=\"badgeInline\"\><span class\=\"offline\" title\=\"offline\"\><\/span\> <span class\=\"aclColor_[0-9]+\"\><a class\=\"plain\" href\=\"\/user\/.+\/\"\>([a-z0-9]+)<\/a>/gi, matches, blockedUsers = [];
+    var searchString = /<span class="aclColor_.*"><a class="plain" href="\/user\/.*\/">([a-z)-9_.-]+)<\/a>/gi, matches, blockedUsers = [];
 
     $.ajax({
         type: "GET",
         url: "/settings/privacy/",
         success: function (data) {
             returnedData = data;
-            while (matches = searchString.exec(returnedData.html)) { // FUCK THIS ERROR, it doesn't matter. :P
+            while (matches = searchString.exec(returnedData.html)) {
                 blockedUsers.push(matches[1]);
             }
             console.log(blockedUsers);
         },
         returnData: "json"
     });
-	
 	return blockedUsers;
 }
