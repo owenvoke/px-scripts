036671ea4015dc66d7113f70debcd712840945ea
PXgamer <owzie123@gmail.com>
2016-05-09 14:49:43 +0100

    Updated to return an object

--- Libraries/Get DMCA'd Torrents.js
+++ Libraries/Get DMCA'd Torrents.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Get DMCA'd Torrents
 // @namespace    PXgamer
-// @version      0.1
+// @version      0.2
 // @description  Get a list of DMCA'd torrents.
 // @author       PXgamer
 // @require      https://code.jquery.com/jquery-1.12.3.min.js
@@ -12,6 +12,7 @@ function getDMCAdTorrents(searchParam) {
     'use strict';
 
     var returnedData;
+    var all = {dmcaList:[], dmcaHashes:[], dmcaURLs:[]};
     var dmcaURLs = [];
     var dmcaHashes = [];
     var searchString = /<i data-name="(.*)" data-hash="(.*)" data-Klink="(https:\/\/kat\.cr\/.*-t[0-9]+\.html)">/ig, matches, dmcaList = [];
@@ -28,10 +29,11 @@ function getDMCAdTorrents(searchParam) {
                 dmcaHashes.push(matches[2]);
                 dmcaURLs.push(matches[3]);
             }
-            console.log(dmcaList);
-            console.log(dmcaHashes);
-            console.log(dmcaURLs);
+            all.dmcaList = dmcaList;
+            all.dmcaHashes = dmcaHashes;
+            all.dmcaURLs = dmcaURLs;
         },
         returnData: "text"
     });
+    return all;
 }
