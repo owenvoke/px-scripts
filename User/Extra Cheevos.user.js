fccf91fcf2c6f1e820c988eaf11814809871abb0
PXgamer <owzie123@gmail.com>
2016-05-09 17:56:49 +0100

    Updated cheevos script

--- User/Extra Cheevos.user.js
+++ User/Extra Cheevos.user.js
@@ -4,7 +4,7 @@
 // @description Adds more cheevos
 // @include     *kat.cr/achievements*
 // @author      PXgamer
-// @version     0.2
+// @version     0.3
 // @grant       none
 // ==/UserScript==
 
@@ -14,19 +14,21 @@ $(function(){
     var i = 0;
     var username = $('#navigation li a span.usernameProfile.menuItem').text();
     var rep = "0";
-    var acl = katuser.acl;
+    var acl = 1;
+    var loc = window.location.href;
 
-    if (location.hrefwindow.location.href.substr(str.length - 14) == "/achievements/") {
+    if (loc.substr(loc.length - 14) == "/achievements/") {
         for (i = 0; i < cheevoTitles.length; i++) {
-            $('table.achTable tr td:nth-child(2)').append(
-                '<li> <span class="achBadge specialAchBack "><a href="/achievements/'+encodeURIComponent(cheevoTitles[i])+'" title=""><span class="specialAchIcon"></span><span class="achTitle">'+cheevoTitles[i]+'</span></a></span> <strong>x 1</strong>  <span class="achDesc">'+cheevoDescriptions[i]+'</span> </li>'
+            $('table.achTable tr td.leftpad20px.width100perc ul').append(
+                '<li> <span class="achBadge specialAchBack assignedAchievement"><a href="/achievements/'+encodeURIComponent(cheevoTitles[i])+'" title=""><span class="specialAchIcon"></span><span class="achTitle">'+cheevoTitles[i]+'</span></a></span> <strong>x 1</strong>  <span class="achDesc">'+cheevoDescriptions[i]+'</span> </li>'
             );
         }
     }
     for (i = 0; i < cheevoTitles.length; i++) {
-        if (location.hrefwindow.location.href.indexOf(encodeURIComponent(cheevoTitles[i])) > -1) {
+        if (loc.indexOf(encodeURIComponent(cheevoTitles[i])) > -1) {
+            document.title = "Achievements - Kickass Torrents";
             $('.errorpage').html('<h2>Users that received <b>'+cheevoTitles[i]+'</b> achievement</h2>'+
-                                 '<a href="/achievements/" class="block clearleft">? Back to all achievements</a>'+
+                                 '<a href="/achievements/" class="block clearleft">← Back to all achievements</a>'+
                                  '<div class="botmarg10px center topmarg10px">'+
                                  '<img src="//kastatic.com/images/achMedal_special.jpg" /> '+
                                  '<div class="ach_desc">'+cheevoDescriptions[i]+'</div>'+
@@ -37,6 +39,7 @@ $(function(){
                                  '<span class="badgeInline"><span class="online" title="online"></span><span class="aclColor_"><a class="plain" href="/user/'+username+'">'+username+'</a></span><span title="Reputation" class="repValue positive">'+rep+'</span></span></li>'
                                 );
             $('.errorpage').css("background", "none");
+            $('.errorpage').removeClass('errorpage');
         }
     }
 });
