c5fac932fc851a9c82ab539fa104f4ed85134d97
PXgamer <owzie123@gmail.com>
2016-05-09 20:17:14 +0100

    Updated script

--- User/Signature Hide on Profiles.user.js
+++ User/Signature Hide on Profiles.user.js
@@ -1,19 +1,19 @@
- ==UserScript==
- @name         Signature Hide on Profiles
- @namespace    PXgamer
- @version      0.1
- @description  Hide signature on profiles
- @author       PXgamer
- @match        kat.cruser
- @grant        none
- ==UserScript==
+// ==UserScript==
+// @name         Signature Hide on Profiles
+// @namespace    PXgamer
+// @version      0.1
+// @description  Hide signature on profiles
+// @author       PXgamer
+// @include      *kat.cr/user/*/
+// @grant        none
+// ==/UserScript==
 
 (function() {
     'use strict';
 
-    $('tr td strongcontains(Signature)').html('spanspan class=ka ka16 ka-eye style=display inline-block; title=Toggle Signature id=toggleSigspanspan style=color black; display inline-block;Signaturespanspan');
+    $('tr td strong:contains("Signature:")').html('<span><span class="ka ka16 ka-eye" style="display: inline-block;" title="Toggle Signature" id="toggleSig"></span><span style="color: black; display: inline-block;">Signature</span></span>');
 })();
 
 $('#toggleSig').on('click', function() {
-    $('#wrapperInner  div.mainpart  table  tbody  tr  tdnth-child(1)  divnth-child(6)  div  div.leftpad10px  table  tbody  trnth-child(6)  tdnth-child(2)').toggle();
+    $('#wrapperInner > div.mainpart > table > tbody > tr > td:nth-child(1) > div:nth-child(6) > div > div.leftpad10px > table > tbody > tr:nth-child(6) > td:nth-child(2)').toggle();
 });
