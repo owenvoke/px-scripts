e5041cbdc960b1c1c1e4f43f213010e0ec1dba7f
PXgamer <owzie123@gmail.com>
2016-05-09 14:43:04 +0100

    Added a search library for DMCA'd torrents

---
+++ Libraries/Get DMCA'd Torrents.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Get DMCA'd Torrents
+// @namespace    PXgamer
+// @version      0.1
+// @description  Get a list of DMCA'd torrents.
+// @author       PXgamer
+// @require      https://code.jquery.com/jquery-1.12.3.min.js
+// @grant        none
+// ==/UserScript==
+
+function getDMCAdTorrents(searchParam) {
+    'use strict';
+
+    var returnedData;
+    var dmcaURLs = [];
+    var dmcaHashes = [];
+    var searchString = /<i data-name="(.*)" data-hash="(.*)" data-Klink="(https:\/\/kat\.cr\/.*-t[0-9]+\.html)">/ig, matches, dmcaList = [];
+
+    $.ajax({
+        method: "GET",
+        url: "https://pxstat.us/misc/dmca/?q=" + encodeURIComponent(searchParam),
+        async: false,
+        success: function (data) {
+            returnedData = data;
+            console.log(returnedData);
+            while (matches = searchString.exec(returnedData)) {
+                dmcaList.push(matches[1]);
+                dmcaHashes.push(matches[2]);
+                dmcaURLs.push(matches[3]);
+            }
+            console.log(dmcaList);
+            console.log(dmcaHashes);
+            console.log(dmcaURLs);
+        },
+        returnData: "text"
+    });
+}
