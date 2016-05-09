08c65ce3d021b00abc7d250fe2bd16a65e7853c5
PXgamer <owzie123@gmail.com>
2016-05-09 22:23:49 +0100

    Changed version number

--- Libraries/Get User Hash.js
+++ Libraries/Get User Hash.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Get User Hash
 // @namespace    PXgamer
-// @version      0.1
+// @version      0.2
 // @description  Get any user's hash, for various activities
 // @author       PXgamer
 // @grant        none
