51109d3993fcdeacba60383e5103fcd21aecd8d8
Owen Voke <owzie123@gmail.com>
2016-05-10 22:50:31 +0100

    Create Add bookmark from browse pages.user.js

---
+++ User/Add bookmark from browse pages.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Add bookmark from browse pages
+// @namespace    PXgamer
+// @version      0.1
+// @description  Adds a bookmark torrent button in any browse page.
+// @author       PXgamer
+// @include      *kat.cr/browse/*
+// @include      *kat.cr/*/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $('a.icon16.askFeedbackjs[title="Torrent magnet link"]').each(function() {
+        var hash = $(this).attr('data-id');
+        $(this).parent().append(
+            '<a class="icon16 ajaxLink" title="Add to bookmarks" href="/bookmarks/add/torrent/'+hash+'/" target="_blank"><i class="ka ka-16 ka-follow"></i></a>'
+        );
+    });
+})();
