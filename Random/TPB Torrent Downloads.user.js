a69f545f40baeebac040ecb50c07ffb08d5b7d53
Owen Voke <owzie123@gmail.com>
2016-05-10 23:15:21 +0100

    Create TPB Torrent Downloads.user.js

---
+++ Random/TPB Torrent Downloads.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         TPB Torrent Downloads
+// @namespace    PXgamer
+// @version      0.1
+// @description  Piratebay download via torrents
+// @author       PXgamer
+// @match        *thepiratebay.se/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $('a[title="Download this torrent using magnet"][href^="magnet:"]').each(function() {
+        var hash = $(this).attr('href').split(":")[3];
+        hash = hash.split("&")[0];
+        $(this).after('<a href="http://torcache.net/torrent/'+hash+'.torrent" title="Download using torrent file"><img src="https://pximg.xyz/images/247c550f6ff9d70161fe85b2b31ecbb2.png" style="width: 12px; height: 12px;" alt="Torrent link"></a>');
+    });
+})();
