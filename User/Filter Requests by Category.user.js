d231575e0a9036a15b7f729d47969c7135ec4c6e
Owen Voke <owzie123@gmail.com>
2016-05-17 14:50:23 +0100

    Create Filter Requests by Category.user.js

---
+++ User/Filter Requests by Category.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Filter Requests by Category
+// @namespace    PXgamer
+// @version      0.1
+// @description  Adds a dropdown to filter the requests search by category.
+// @author       PXgamer
+// @include      *kat.cr/request/search/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $('.buttonsline.floatleft').append(' <select class="sortReqsOpt" style="letter-spacing: 0px; width: 160px !important;"><option value="All">All</option><option value="Movies">Movies</option><option value="TV">TV</option><option value="Music">Music</option><option value="Games">Games</option><option value="Applications">Applications</option><option value="Books">Books</option><option value="Anime">Anime</option><option value="Other">Other</option><option value="XXX">XXX</option><option value="Unassigned">Unassigned</option></select> <button class="postLink icon16 textButton sortReqs">');
+
+    $('.sortReqs').on('click', function() {
+        $('.request-item.ideaBox').show();
+        if ($('.sortReqsOpt').val() != 'All') {
+            $('h6.lightgrey a[href^="/request/popular/"]').each(function() {
+                if ($(this).text() != $('.sortReqsOpt').val()) {
+                    $(this).parent().parent().parent().hide();
+                }
+            });
+        }
+    });
+})();
