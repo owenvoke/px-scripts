a9dbe1bf2f2d3748ca82798757068c9f8c6ef642
Owen Voke <owzie123@gmail.com>
2016-05-17 14:55:46 +0100

    Update Filter Requests by Category.user.js

--- User/Filter Requests by Category.user.js
+++ User/Filter Requests by Category.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Filter Requests by Category
 // @namespace    PXgamer
-// @version      0.1
+// @version      0.2
 // @description  Adds a dropdown to filter the requests search by category.
 // @author       PXgamer
 // @include      *kat.cr/request/search/*
@@ -11,7 +11,7 @@
 (function() {
     'use strict';
 
-    $('.buttonsline.floatleft').append(' <select class="sortReqsOpt" style="letter-spacing: 0px; width: 160px !important;"><option value="All">All</option><option value="Movies">Movies</option><option value="TV">TV</option><option value="Music">Music</option><option value="Games">Games</option><option value="Applications">Applications</option><option value="Books">Books</option><option value="Anime">Anime</option><option value="Other">Other</option><option value="XXX">XXX</option><option value="Unassigned">Unassigned</option></select> <button class="postLink icon16 textButton sortReqs">');
+    $('.buttonsline.floatleft').append(' <select class="sortReqsOpt" style="vertical-align: middle;letter-spacing: 0px; width: 160px !important;"><option value="All">All</option><option value="Movies">Movies</option><option value="TV">TV</option><option value="Music">Music</option><option value="Games">Games</option><option value="Applications">Applications</option><option value="Books">Books</option><option value="Anime">Anime</option><option value="Other">Other</option><option value="XXX">XXX</option><option value="Unassigned">Unassigned</option></select> <button class="siteButton bigButton sortReqs">Filter</button>');
 
     $('.sortReqs').on('click', function() {
         $('.request-item.ideaBox').show();
