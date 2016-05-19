c005e322774bbb0dc2ed0c3ea33d784fc9a31b14
Owen Voke <owzie123@gmail.com>
2016-05-19 22:45:42 +0100

    Update Image Favourites.user.js

--- User/Image Favourites.user.js
+++ User/Image Favourites.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Image Favourites
 // @namespace    PXgamer
-// @version      0.1
+// @version      0.2
 // @description  Add favourites for images.
 // @author       PXgamer
 // @include      *kat.cr/user/*/albums/
@@ -14,7 +14,11 @@
 (function() {
     'use strict';
 
-    $('a.turnoverButton.siteButton.bigButton[href$="/albums/"]').before('<a class="turnoverButton siteButton bigButton favouriteImages">Favourites</a>');
+    var dataVal = GM_getValue('favouriteImagesData', '[]');
+    var data = JSON.parse(dataVal);
+    var htmlD = '';
+
+    $('a.turnoverButton.siteButton.bigButton[href$="/albums/"]').before('<a class="turnoverButton siteButton bigButton favouriteImages">Favourites</a><a class="favouritePop" style="display: none;"></a>');
     if (location.href.indexOf('/recentimages/') > -1) {
         $('a[href^="/image/delete/"][data-id].deleteImageJs').after('<a class="favouriteImageJS topmarg2px absolute" style="margin-left: 25px;"><i class="ka ka16 ka-star"></i></a>');
     }
@@ -28,20 +32,38 @@
         var imgCan = $('div.galleryThumbSizerStills.inlineblock:first').parent();
         imgCan.html('');
 
-        var imgList = GM_getValue('favImgList', '');
-
-        if (imgList.length === 0) {
+        if (data.length === 0) {
             imgCan.append('<h3>No Favourites Were Found!</h3>');
         }
         else {
-            for (var i = 0; i < imgList.length; i++) {
-                imgCan.append('<div class="galleryThumbSizerStills inlineblock ui-selectee">\
-<a class="deleteImageJs topmarg2px leftmarg2px absolute favRemove"><i class="ka ka16 ka-delete ka-green"></i></a>\
-<a href="'+imgList[i].url+'" class="galleryThumb ajaxLink" rel="recentimages">\
-<img class="lazyjs" data-original="'+imgList[i].url+'" src="'+imgList[i].url+'" style="display: inline;">\
+            for (var i = 0; i < data.length; i++) {
+                imgCan.append('<div class="galleryThumbSizerStills inlineblock">\
+<a class="topmarg2px leftmarg2px absolute favRemove"><i class="ka ka16 ka-delete ka-green"></i></a>\
+<a data-href="'+data[i].imageUrl+'" class="galleryThumb favouritePop">\
+<img class="lazyjs" data-id="'+data[i].imageId+'" data-original="'+data[i].imageUrl+'" src="'+data[i].imageUrl+'" style="display: inline;" />\
 </a>\
 </div>');
             }
         }
+        $('a.favouritePop').css('cursor', 'pointer');
+
+        $('a.favouritePop').on('click', function() {
+            var imgLoc = $(this).attr('data-href');
+            var w = ($( window ).width()/100*80);
+            var h = ($( window ).height()/100*80);
+            htmlD = '<div style="width: '+w+'px; height: '+h+'px; overflow: auto; scroll: hidden; position:relative;"><img src="'+imgLoc+'" style="width: '+(w-5)+'px; height: '+(h-5)+'px;" /></div>';
+            $.fancybox(htmlD);
+        });
+    });
+
+    $('a.favouriteImageJS').on('click', function() {
+        var dataId   = $(this).prev('a.deleteImageJs').attr('data-id');
+        var imageUrl = $(this).next('a.galleryThumb').attr('href');
+        var dataArr = {
+            'imageId':   dataId,
+            'imageUrl':  imageUrl
+        };
+        data.push(dataArr);
+        GM_setValue('favouriteImagesData', JSON.stringify(data));
     });
 })();
