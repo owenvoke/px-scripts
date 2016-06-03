6753de9b581f007445d18ced1267595d64602807
Owen Voke <owzie123@gmail.com>
2016-06-03 11:21:15 +0100

    Update Image BG Toggle.user.js
    Added support for grey

--- Random/Image BG Toggle.user.js
+++ Random/Image BG Toggle.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Image BG Toggle
 // @namespace    PXgamer
-// @version      0.4
+// @version      0.5
 // @description  Toggles the background colour for images in the browser
 // @author       PXgamer
 // @include      *.png*
@@ -14,17 +14,29 @@
 
 (function() {
     'use strict';
-
-    var col = false;
+    // EDITABLE VARS
+    var allowGrey = false;
+    
+    // DO NOT EDIT BELOW
+    var col = 'black';
     $('body').append('<span class="bgToggle" style="cursor: pointer; -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; z-index: 999999; float: right; background-color: white; border: 1px solid grey; border-radius: 2px; margin: 5px; padding: 2px; font-family: fantasy;">TOGGLE BG</span>');
     $('.bgToggle').on('click', function() {
-        if (!col) {
-            $('body').css('background-color', 'black');
-            col = true;
+        if (col == 'black') {
+            $('body').css('background-color', col);
+            if (allowGrey) { col = 'grey'; }
+            else { col = 'white'; }
+        }
+        else if (col == 'grey') {
+            $('body').css('background-color', col);
+            col = 'white';
+        }
+        else if (col == 'white') {
+            $('body').css('background-color', col);
+            col = 'black';
         }
         else {
             $('body').css('background-color', 'white');
-            col = false;
+            col = 'black';
         }
     });
 })();
