9d108cb87155c199a7ecd0cd6898741d532dfa38
Owen Voke <owzie123@gmail.com>
2016-06-04 12:07:17 +0100

    Create User RSS on Profile.user.js

---
+++ User/User RSS on Profile.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         User RSS on Profile
+// @namespace    PXgamer
+// @version      0.1
+// @description  Adds user uploads RSS button to their profile.
+// @author       PXgamer
+// @include      *kat.cr/user/*/
+// @grant        none
+// ==/UserScript==
+/*jshint multistr: true */
+
+(function() {
+    'use strict';
+
+    var nick = $('h1.nickname').html().split('<span ')[0].trim();
+    if (nick == $('span.usernameProfile.menuItem').text()) {
+        $('div.profileBody').before('<div class="botmarg10px" style="margin-left: 5px"><a href="http://kat.cr/usearch/user%3A'+nick+'/?rss=1" target="_blank" class="kaButton smallButton normalText ka-red"><i class="ka ka-icon16 ka-rss"></i> My Upload RSS</a></div>');
+    }
+    else {
+        $('div.userPic.floatleft.userPicSize100px').next('div.botmarg10px').append('<a href="http://kat.cr/usearch/user%3A'+nick+'/?rss=1" target="_blank" class="ka-red kaButton smallButton normalText"><i class="ka ka-icon16 ka-rss"></i> User Upload RSS</a>');
+    }
+})();
