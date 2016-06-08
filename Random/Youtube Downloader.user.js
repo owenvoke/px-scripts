9578f08ccd7be3d8f81dd1d9265bbb0698e8b515
Owen Voke <owzie123@gmail.com>
2016-06-08 11:02:53 +0100

    Create Youtube Downloader.user.js

---
+++ Random/Youtube Downloader.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Youtube Downloader
+// @namespace    PXgamer
+// @version      0.1
+// @description  Allows you to download videos off YouTube. (Yet another one...)
+// @author       PXgamer
+// @include      *youtube.com/watch?v=*
+// @require      https://code.jquery.com/jquery-1.12.3.min.js
+// @grant        none
+// ==/UserScript==
+/*jshint multistr: true */
+
+(function() {
+    'use strict';
+
+    $('#watch8-secondary-actions').prepend('<div class="yt-uix-menu dlmp3"><button class="yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon no-icon-markup pause-resume-autoplay yt-uix-menu-trigger yt-uix-tooltip" type="button" role="button" aria-pressed="false" title="Download as MP3" aria-haspopup="true" data-tooltip-text="Add to" aria-labelledby="yt-uix-tooltip2707-arialabel"><span><img style="width: 20px; height: 20px;" src="https://pximg.xyz/assets/img/cloud_download.svg"></span><span class="yt-uix-button-content">Download as MP3</span></button></div>');
+
+    $('.dlmp3').on('click', function() {
+        var url = 'https://www.youtubeinmp3.com/fetch/?format=JSON&video=' + location.href;
+        $.ajax({
+            method: "GET",
+            url: url,
+            dataType: "json",
+            success: function(data) {
+                location.href = data.link;
+            }
+        });
+    });
+
+})();
