9e0d73dab494206d14ca8b6d6b015cf923b0dbd9
Owen Voke <owzie123@gmail.com>
2016-06-10 22:51:24 +0100

    Update Draggable Sidebar Elements.user.js

--- User/Draggable Sidebar Elements.user.js
+++ User/Draggable Sidebar Elements.user.js
@@ -4,81 +4,74 @@
 // @include     *kat.cr/*
 // @exclude     *kat.cr/settings*
 // @description Fully mushable sidebar
-// @version     1.9
-// @grant       none
+// @version     2.0
+// @grant       GM_setValue
+// @grant       GM_getValue
 // ==/UserScript==
 
-// set the cookie expiry time (days):
-var setCookieExpiry = 2000;
 
-
+var setSelector = "#sidebar";
+var setCookieName = "sidebarOrder";
+console.log(GM_getValue(setCookieName, ''));
 $(".sliderbox").css('padding', '3px');
 
 var i = 0;
 var s = $(".sliderbox");
 $(".sliderbox").each( function() {
-$(this).wrap('<li id="item-'+i+'" style="margin-bottom:2px"></li>');
-i++;
+    $(this).wrap('<li id="item-'+i+'" style="margin-bottom:2px"></li>');
+    i++;
 });
 
 $('#sidebar').sortable({ containment: "#sidebar", scroll: false });
 
-var setSelector = "#sidebar";
-var setCookieName = "sideOrder";
- 
 function getOrder() {
-
-	$.cookie(setCookieName, $(setSelector).sortable("toArray"), { expires: setCookieExpiry, path: "/" });
+    GM_setValue(setCookieName, $(setSelector).sortable("toArray"));
 }
 
 function restoreOrder() {
-	var list = $(setSelector);
-	if (list == null) return
- 
-	var cookie = $.cookie(setCookieName);
-	if (!cookie) return;
+    var list = $(setSelector);
+    if (list === null) return false;
 
-	var IDs = cookie.split(",");
- 
+    var cookie = GM_getValue(setCookieName, '');
+    if (!cookie) return false;
 
-	var items = list.sortable("toArray");
- 
+    var IDs = cookie;
 
-	var rebuild = new Array();
-	for ( var v=0, len=items.length; v<len; v++ ){
-		rebuild[items[v]] = items[v];
-	}
- 
-	for (var i = 0, n = IDs.length; i < n; i++) {
- 
+    var items = list.sortable("toArray");
 
-		var itemID = IDs[i];
- 
-		if (itemID in rebuild) {
- 
+    var rebuild = [];
+    for ( var v=0, len=items.length; v<len; v++ ){
+        rebuild[items[v]] = items[v];
+    }
 
-			var item = rebuild[itemID];
- 
+    for (var i = 0, n = IDs.length; i < n; i++) {
 
-			var child = $("#sidebar.ui-sortable").children("#" + item);
- 
+        var itemID = IDs[i];
 
-			var savedOrd = $("#sidebar.ui-sortable").children("#" + itemID);
- 
+        if (itemID in rebuild) {
 
-			child.remove();
- 
-			$("#sidebar.ui-sortable").filter(":first").append(savedOrd);
-		}
-	}
+            var item = rebuild[itemID];
+
+            if (item !== '') {
+                var child = $("#sidebar.ui-sortable").children("#" + item);
+
+                var savedOrd = $("#sidebar.ui-sortable").children("#" + itemID);
+
+                child.remove();
+
+                $("#sidebar.ui-sortable").filter(":first").append(savedOrd);
+            }
+        }
+    }
 }
- 
-jQuery(document).ready(function() {
-	jQuery("#sidebar").sortable({axis: "y",
-		cursor: "move",
-		update: function() { getOrder(); }
-	});
- 
 
-	restoreOrder();
+jQuery(document).ready(function() {
+    jQuery("#sidebar").sortable(
+        {axis: "y",
+         cursor: "move",
+         update: function() { getOrder(); }
+        }
+    );
+
+    restoreOrder();
 });
