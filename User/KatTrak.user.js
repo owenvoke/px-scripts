58c1adbf06c672c170590fd88af4ecf677e46500
Owen Voke <owzie123@gmail.com>
2016-06-19 21:06:53 +0100

    Update KatTrak.user.js

--- User/KatTrak.user.js
+++ User/KatTrak.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KatTrak
 // @namespace    PXgamer
-// @version      0.3
+// @version      0.4
 // @description  A Trakt system for integrating with Kickass Torrents.
 // @author       PXgamer
 // @include      *kat.cr/*
@@ -53,17 +53,49 @@
         }
     }
     if (getURL.indexOf('pxgamer.github.io') > -1) {
-        if (auth_code !== '') {
+        var logged_in_valid = false;
+        $.ajax({
+            beforeSend: function (request)
+            {
+                request.setRequestHeader("Content-type", "application/json");
+                request.setRequestHeader("trakt-api-key", "9efcadc5be0011a406fa0819192bd3aef0b3b2d9fa6ba90f3ffd3907138195d3");
+                request.setRequestHeader("trakt-api-version", 2);
+                request.setRequestHeader("Authorization", "Bearer "+auth_code+"");
+            },
+            type: "GET",
+            async: false,
+            url: "https://api-v2launch.trakt.tv/sync/last_activities",
+            success: function (data) {
+                if (data.all !== '') {
+                    logged_in_valid = true;
+                }
+            },
+            returnData: "json"
+        });
+        if (auth_code !== '' && logged_in_valid) {
             $('.checkup-box').html('<table style="margin-left: 20%;"><tr style="text-align: left;"><td>Status:</td><td style="padding: 15px"></td><td>Success</td></tr><tr style="text-align: left;"><td>Auth Code:</td><td style="padding: 15px"></td><td>' + auth_code + '</td></tr></table>');
+            $('.unauthKt').replaceWith('<button class="btn btn-lg btn-danger unauthKt" type="button">Unauthorise KatTrak</button>');
+            $('.unauthKt').on('click', function() {
+                GM_setValue('katTrakAuth', '');
+                location.reload();
+            });
+        }
+        else if (auth_code !== '') {
+            $('.checkup-box').html('<table style="margin-left: 20%;"><tr style="text-align: left;"><td>Status:</td><td style="padding: 15px"></td><td>Failed: Invalid Auth Code</td></tr><tr style="text-align: left;"><td>Auth Code:</td><td style="padding: 15px"></td><td>' + auth_code + '</td></tr></table>');
+            $('.unauthKt').replaceWith('<button class="btn btn-lg btn-primary unauthKt" type="button">Re-Authorise KatTrak</button>');
+            $('.unauthKt').on('click', function() {
+                GM_setValue('katTrakAuth', '');
+                location.href = 'https://trakt.tv/oauth/authorize?client_id=9efcadc5be0011a406fa0819192bd3aef0b3b2d9fa6ba90f3ffd3907138195d3&redirect_uri=https%3A%2F%2Fpxstat.us%2Ftrakt%2F&response_type=code';
+            });
         }
         else {
-            $('.checkup-box').html('<table style="margin-left: 20%;"><tr style="text-align: left;"><td>Status:</td><td style="padding: 15px"></td><td>Failed</td></tr><tr style="text-align: left;"><td>Auth Code:</td><td style="padding: 15px"></td><td>' + auth_code + '</td></tr></table>');
+            $('.checkup-box').html('<table style="margin-left: 20%;"><tr style="text-align: left;"><td>Status:</td><td style="padding: 15px"></td><td>Failed: No Code Provided</td></tr><tr style="text-align: left;"><td>Auth Code:</td><td style="padding: 15px"></td><td>' + auth_code + '</td></tr></table>');
+            $('.unauthKt').replaceWith('<button class="btn btn-lg btn-primary unauthKt" type="button">Authorise KatTrak</button>');
+            $('.unauthKt').on('click', function() {
+                GM_setValue('katTrakAuth', '');
+                location.href = 'https://trakt.tv/oauth/authorize?client_id=9efcadc5be0011a406fa0819192bd3aef0b3b2d9fa6ba90f3ffd3907138195d3&redirect_uri=https%3A%2F%2Fpxstat.us%2Ftrakt%2F&response_type=code';
+            });
         }
-        // Unauth code
-        $('.unauthKt').on('click', function() {
-            GM_setValue('katTrakAuth', '');
-            location.reload();
-        });
     }
     if (getURL.indexOf('kat.cr') > -1 && getURL.indexOf('.html') > -1) {
         var category = $('span[id^="cat_"] strong a[href]:first').text();
