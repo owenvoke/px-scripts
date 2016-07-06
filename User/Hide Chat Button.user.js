be4f0663b6519cef5274f44e421276132888a828
Owen Voke <owzie123@gmail.com>
2016-07-06 18:00:22 +0100

    Create Hide Chat Button.user.js

---
+++ User/Hide Chat Button.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Hide Chat Button
+// @namespace    PXgamer
+// @version      0.1
+// @description  Hides the chat button.
+// @author       PXgamer
+// @include      *kat.cr/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $('.chatButton').hide();
+})();
