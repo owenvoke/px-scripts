09860a3090f20f2eb123659fda2149411ee76fea
Owen Voke <owzie123@gmail.com>
2016-07-17 11:14:09 +0100

    Update Extra Smileys.user.js

--- User/Extra Smileys.user.js
+++ User/Extra Smileys.user.js
@@ -2,34 +2,33 @@
 // @name        Extra Smileys
 // @namespace   PXgamer
 // @description Adds more smileys to KAT
-// @include     *localhost:999/kat/testpage
 // @include     *kickass.to/*
 // @include     *kat.cr/*
-// @version     2.6
+// @author      PXgamer
+// @version     2.9
 // @grant       none
 // ==/UserScript==
 
 
 $(window).load(function(){
-  
-	$('.bbedit-smileybar').append('<img title="Smoking" class="cusSmile" alt="smoke" href src="http://www.skype-emoticons.com/images/emoticon-00176-smoke.gif" />');
-	$('.bbedit-smileybar').append('<img title="Headbanger" class="cusSmile" alt="headbang" src="http://www.skype-emoticons.com/images/emoticon-00179-headbang.gif" />');
-	$('.bbedit-smileybar').append('<img title="Mooning" class="cusSmile" alt="mooning" src="http://www.skype-emoticons.com/images/emoticon-00172-mooning.gif" />');
-	$('.bbedit-smileybar').append('<img title="Squirrel" class="cusSmile" alt="squirrel" src="http://www.skype-emoticons.com/images/emoticon-00185-heidy.gif" />');
-	$('.bbedit-smileybar').append('<img title="Ninja" class="cusSmile" alt="ninja" src="http://www.skype-emoticons.com/images/emoticon-00170-ninja.gif" />');
-	$('.bbedit-smileybar').append('<img title="Beer" class="cusSmile" alt="beer" src="http://www.skype-emoticons.com/images/emoticon-00167-beer.gif" />');
-	$('.bbedit-smileybar').append('<img title="Drink" class="cusSmile" alt="drink" src="http://www.skype-emoticons.com/images/emoticon-00168-drink.gif" />');
-	$('.bbedit-smileybar').append('<img title="Cake" class="cusSmile" alt="cake" src="http://www.skype-emoticons.com/images/emoticon-00166-cake.gif" />');
-	$('.bbedit-smileybar').append('<img title="Pizza" class="cusSmile" alt="pizza" src="http://www.skype-emoticons.com/images/emoticon-00163-pizza.gif" />');
-	$('.bbedit-smileybar').append('<img title="Rain" class="cusSmile" alt="rain" src="http://www.skype-emoticons.com/images/emoticon-00156-rain.gif" />');
-	$('.bbedit-smileybar').append('<img title="Mail" class="cusSmile" alt="mail" src="http://www.skype-emoticons.com/images/emoticon-00154-mail.gif" />');
-	$('.bbedit-smileybar').append('<img title="Music" class="cusSmile" alt="music" src="http://www.skype-emoticons.com/images/emoticon-00159-music.gif" />');
-	$('.bbedit-smileybar').append('<img title="Phone" class="cusSmile" alt="phone" src="http://www.skype-emoticons.com/images/emoticon-00161-phone.gif" />');
-	$('.bbedit-smileybar').append('<img title="Weed" class="cusSmile" alt="weed" src="http://www.sherv.net/cm/emo/smoke/weed-4.gif" height="19px" width="19px" />');
-	$('.bbedit-smileybar').append('<img title="MLP Headless" class="cusSmile" alt="mlph" src="http://fc09.deviantart.net/fs70/f/2014/251/3/8/_gift__club_can_t_handle_me__smiley__by_ricepoison-d7yhrlf.gif" height="19px" width="19px" />');
-	$('.bbedit-smileybar').append('<img title="Coffee" class="cusSmile" alt="coffee" src="http://emoticoner.com/files/emoticons/skype_smileys/coffee-skype-smiley.gif" />');
-	
-	
+	$('.bbedit-smileybar').append('<img title="Smoking" class="cusSmile" alt="smoke" src="https://pximg.xyz/images/e3f6c9546b0b85138856a654b6acee85.gif" />');
+	$('.bbedit-smileybar').append('<img title="Headbanger" class="cusSmile" alt="headbang" src="https://pximg.xyz/images/96c98c900ddcd6c8b14177ffba06ed13.gif" />');
+	$('.bbedit-smileybar').append('<img title="Mooning" class="cusSmile" alt="mooning" src="https://pximg.xyz/images/4bb967650d41ff6bcd58ac203a797235.gif" />');
+	$('.bbedit-smileybar').append('<img title="Squirrel" class="cusSmile" alt="squirrel" src="https://pximg.xyz/images/3bd96c8fcc699c381f6263e295ad225b.gif" />');
+	$('.bbedit-smileybar').append('<img title="Ninja" class="cusSmile" alt="ninja" src="https://pximg.xyz/images/0b914e135932cd0da66df267f2d84b34.gif" />');
+	$('.bbedit-smileybar').append('<img title="Beer" class="cusSmile" alt="beer" src="https://pximg.xyz/images/6ad8bb13eb3b86c55f624073fd229599.gif" />');
+	$('.bbedit-smileybar').append('<img title="Drink" class="cusSmile" alt="drink" src="https://pximg.xyz/images/8a6854009cf40e59a13fc22da1e2f8ae.gif" />');
+	$('.bbedit-smileybar').append('<img title="Cake" class="cusSmile" alt="cake" src="https://pximg.xyz/images/9f53064bb76f3774b1c6247453afc715.gif" />');
+	$('.bbedit-smileybar').append('<img title="Pizza" class="cusSmile" alt="pizza" src="https://pximg.xyz/images/7edc71a4e28a5b9aabb6e9aaec406a9a.gif" />');
+	$('.bbedit-smileybar').append('<img title="Rain" class="cusSmile" alt="rain" src="https://pximg.xyz/images/b7a972fb35d04e321f553f29a31899c8.gif" />');
+	$('.bbedit-smileybar').append('<img title="Mail" class="cusSmile" alt="mail" src="https://pximg.xyz/images/0637181b7e39d789e56e45806bc6ac9e.gif" />');
+	$('.bbedit-smileybar').append('<img title="Music" class="cusSmile" alt="music" src="https://pximg.xyz/images/16457a3f98d886e7ecb1578f954b38c7.gif" />');
+	$('.bbedit-smileybar').append('<img title="Phone" class="cusSmile" alt="phone" src="https://pximg.xyz/images/2534b7ba848c018453470f35d939abd3.gif" />');
+	$('.bbedit-smileybar').append('<img title="Weed" class="cusSmile" alt="weed" src="https://pximg.xyz/images/95b0ba9220b7b028454762061b3de42b.gif" height="19px" width="19px" />');
+	$('.bbedit-smileybar').append('<img title="MLP Headless" class="cusSmile" alt="mlph" src="https://pximg.xyz/images/0b1b423c2a1bde8e3d1f50d125c4808b.gif" height="19px" width="19px" />');
+	$('.bbedit-smileybar').append('<img title="Coffee" class="cusSmile" alt="coffee" src="https://pximg.xyz/images/b9587160dd9dd695e26583af17287561.gif" />');
+    $('.bbedit-smileybar').append('<img title="Puke" class="cusSmile" alt="puke" src="https://pximg.xyz/images/bf20c58502857123051dc81c7aa3d9be.gif" height="19px" width="19px" />');
+
 jQuery.fn.extend({
 insertAtCaret: function(myValue){
   return this.each(function(i) {
@@ -58,6 +57,5 @@ insertAtCaret: function(myValue){
 }
 });
 
-	$('.cusSmile').click(function(){ $('textarea',$(this).closest('form')).insertAtCaret( '[img width="19px" height="19px"]'+ $(this).attr('src') +'[/img]' );})
-  
+	$('.cusSmile').click(function(){ $('textarea',$(this).closest('form')).insertAtCaret( '[img width="19px" height="19px"]'+ $(this).attr('src') +'[/img]' );});
 });
