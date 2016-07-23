ea84ee45d20b9b21d48a48070f413f738d83883b
Owen Voke <owzie123@gmail.com>
2016-07-23 16:50:19 +0100

    Create Change Discord User Colour.user.js

---
+++ Discord/Change Discord User Colour.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Change Discord User Colour
+// @namespace    PXgamer
+// @version      0.1
+// @description  Change colour of a username in Discord.
+// @author       PXgamer
+// @include      *discordapp.com/channels/*
+// @require      https://code.jquery.com/jquery-3.1.0.min.js
+// @grant        none
+// ==/UserScript==
+/*jshint multistr: true */
+
+(function() {
+    'use strict';
+
+    var username = 'pxgamer'; // Username
+    var colour   = 'aqua';    // Colour to set the users name to.
+    var timetowait = 5;       // Time to wait in seconds, depends on your internet speed.
+
+    setTimeout(function() {
+
+        $('.user-name:contains("'+username+'")').each(function(){$(this).css('color',colour);});
+        console.log('Complete');
+    }, timetowait*1000);
+})();
