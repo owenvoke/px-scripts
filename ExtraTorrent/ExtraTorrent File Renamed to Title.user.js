fa32e78aa644181910284d2a632ea7e565cb84c8
Owen Voke <owzie123@gmail.com>
2016-07-26 13:04:17 +0100

    Update ExtraTorrent File Renamed to Title.user.js

--- ExtraTorrent/ExtraTorrent File Renamed to Title.user.js
+++ ExtraTorrent/ExtraTorrent File Renamed to Title.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         ExtraTorrent File Renamed to Title
 // @namespace    PXgamer
-// @version      0.1
+// @version      0.2
 // @description  Renames ET downloads to the actual file name, instead of hash.
 // @author       PXgamer
 // @include      *extratorrent.cc/torrent_download/*/*.torrent
+// @require      https://code.jquery.com/jquery-3.1.0.min.js
 // @grant        none
 // ==/UserScript==
 /*jshint multistr: true */
