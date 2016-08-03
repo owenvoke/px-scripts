1d0f60f4ebf7ae278f0d18bc33c7c9b3909861dd
Owen Voke <owzie123@gmail.com>
2016-08-03 18:56:38 +0100

    Update ExtraTorrent File Renamed to Title.user.js

--- ExtraTorrent/ExtraTorrent File Renamed to Title.user.js
+++ ExtraTorrent/ExtraTorrent File Renamed to Title.user.js
@@ -1,19 +1,20 @@
 // ==UserScript==
-// @name         ExtraTorrent File Renamed to Title
+// @name         [Extratorrent.cc] Quick Download
 // @namespace    PXgamer
-// @version      0.2
-// @description  Renames ET downloads to the actual file name, instead of hash.
+// @version      0.3
+// @description  Converts all ad download links to direct download.
 // @author       PXgamer
-// @include      *extratorrent.cc/torrent_download/*/*.torrent
+// @include      *extratorrent.cc/*
 // @require      https://code.jquery.com/jquery-3.1.0.min.js
 // @grant        none
 // ==/UserScript==
-/*jshint multistr: true */
 
 (function() {
     'use strict';
 
-    $('a[alt^="Download Torrent"][href^="/download/"]').each(function(){
-        $(this).attr('download', $('h1').text() + '.torrent');
+    $('a[href^="/torrent_download/"]').each(function() {
+        var torr_id  = $(this).attr("href").split("/")[2];
+        var new_url  = "/download/" + torr_id;
+        $(this).attr("href", new_url);
     });
 })();
