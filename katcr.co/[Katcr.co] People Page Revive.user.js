b285d241f4726b2444d61c1805e7df44d65f5936
Owen Voke <owzie123@gmail.com>
2016-08-03 23:30:38 +0100

    Update [Katcr.co] People Page Revive.user.js

--- katcr.co/[Katcr.co] People Page Revive.user.js
+++ katcr.co/[Katcr.co] People Page Revive.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [Katcr.co] People Page Revive
 // @namespace    PXgamer
-// @version      0.2
+// @version      0.3
 // @description  Makes the people page thread like the old site again.
 // @author       PXgamer
 // @include      *katcr.co/community/index.php/topic,177*
@@ -14,28 +14,28 @@
     'use strict';
 
     var titles = {
-        lower: $('#msg_964 > table > tbody > tr:nth-child(1) > td:nth-child(2)').html(),
-        middle: $('#msg_964 > table > tbody > tr:nth-child(1) > td:nth-child(21)').html(),
-        upper: $('#msg_964 > table > tbody > tr:nth-child(1) > td:nth-child(52)').html()
+        lower: '<span style="font-size: 12pt;" class="bbc_size"><strong>Forum Moderators</strong></span>',
+        middle: '<span style="font-size: 12pt;" class="bbc_size"><strong>Super Moderators</strong></span>',
+        upper: '<span style="font-size: 12pt;" class="bbc_size"><strong>Site Administrators</strong></span>'
     };
 
     var people = {
-        lower: $('#msg_964 > table > tbody > tr:nth-child(2) > td:nth-child(2)').html(),
-        middle: $('#msg_964 > table > tbody > tr:nth-child(2) > td:nth-child(21)').html(),
-        upper: $('#msg_964 > table > tbody > tr:nth-child(2) > td:nth-child(52)').html()
+        lower: $('#msg_964 > div:nth-child(6) > table > tbody > tr:nth-child(2) > td:nth-child(2)').html(),
+        middle: $('#msg_964 > div:nth-child(6) > table > tbody > tr:nth-child(2) > td:nth-child(21)').html(),
+        upper: $('#msg_964 > div:nth-child(6) > table > tbody > tr:nth-child(2) > td:nth-child(52)').html()
     };
 
     // Append titles
-    $('#msg_964 > table > tbody > tr:nth-child(1)').empty();
-    $('#msg_964 > table > tbody > tr:nth-child(1)').append('<td class="p1p">'+titles.lower+'</td><td class="p1p">'+titles.middle+'</td><td class="p1p">'+titles.upper+'</td>');
+    $('#msg_964 > div:nth-child(6) > table > tbody > tr:nth-child(1)').empty();
+    $('#msg_964 > div:nth-child(6) > table > tbody > tr:nth-child(1)').append('<td class="p1p">'+titles.lower+'</td><td class="p1p">'+titles.middle+'</td><td class="p1p">'+titles.upper+'</td>');
 
     // Append Users
-    $('#msg_964 > table > tbody > tr:nth-child(2)').empty();
-    $('#msg_964 > table > tbody > tr:nth-child(2)').append('<td class="p1p">'+titles.lower+'<br/>'+people.lower+'</td><td class="p1p">'+titles.middle+'<br/>'+people.middle+'</td><td class="p1p">'+titles.upper+'<br/>'+people.upper+'</td>');
-    $('#msg_964 > table > tbody > tr:nth-child(1)').empty();
+    $('#msg_964 > div:nth-child(6) > table > tbody > tr:nth-child(2)').empty();
+    $('#msg_964 > div:nth-child(6) > table > tbody > tr:nth-child(2)').append('<td class="p1p">'+titles.lower+'<br/>'+people.lower+'</td><td class="p1p">'+titles.middle+'<br/>'+people.middle+'</td><td class="p1p">'+titles.upper+'<br/>'+people.upper+'</td>');
+    $('#msg_964 > div:nth-child(6) > table > tbody > tr:nth-child(1)').empty();
     // Theming
     $('.p1p').css('padding', '2%');
     $('.bbc_link').css('border', 'none');
-    $('#msg_964 > table > tbody tr').css('background', 'none');
+    $('#msg_964 > div:nth-child(6) > table > tbody tr').css('background', 'none');
     $('#msg_964 > table').css('margin', 'auto');
 })();
