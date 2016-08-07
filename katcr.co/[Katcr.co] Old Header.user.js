d5689d14936fb5e824f456abe29cc81429641569
Owen Voke <owzie123@gmail.com>
2016-08-07 19:07:48 +0100

    Update [Katcr.co] Old Header.user.js

--- katcr.co/[Katcr.co] Old Header.user.js
+++ katcr.co/[Katcr.co] Old Header.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [Katcr.co] Old Header
 // @namespace    PXgamer
-// @version      0.5
+// @version      0.6
 // @description  Updates the header to be like the old one.
 // @author       PXgamer
 // @include      *katcr.co/community/*
@@ -21,7 +21,8 @@
     var menu = {
         avatar: $('#upper_section .user .avatar img.avatar').attr('src'),
         bar: $('#main_menu').html(),
-        sticky: ''
+        sticky: '',
+        m_count: $('#button_pm a span strong').text()
     };
 
     var links = {
@@ -49,4 +50,9 @@
     $('#button_pm ul li:first a').html('<i class="ka ka16 ka-list2 lower"></i>Dialogs');
     $('#button_pm ul li:last a').html('<i class="ka ka16 ka-message lower"></i>Create new');
     $('#menu_nav').append('<li id="button_user"> <a class="firstlevel" href="'+links.profile+'"><img src="'+menu.avatar+'" style="width:40px; height: 40px; border-radius: 50%; border: 2px solid #2c240f; margin: 5% auto; padding: 0;"></a> <ul style="right: 0;"><li> <a href="'+links.replies+'"><span class="last">Latest Replies</span></a></li><li><a href="'+links.forumset+'"><span>Forum Settings</span></a> </li><li> <a href="'+links.settings+'"><span class="last">Settings</span></a></li><li> <a href="'+links.logout+'"><span class="last">Logout</span></a></li>');
+    if (menu.m_count !== '') {
+        $('#button_pm a span').html('My Messages');
+        $('#button_pm a').css('line-height', '3.9em');
+        $('#button_pm a span').append(' <strong><sup class="" style="color: #FFA500;"><strong style="border-radius:50%; min-width: 10px; min-height: 10px; font-size: 18px; vertical-align: bottom;">*</strong>'+menu.m_count+'</sup></strong>');
+    }
 })();
