0b7639a3bed6287265e4429b5b9d327070d83b54
Owen Voke <owzie123@gmail.com>
2016-08-18 13:08:04 +0100

    Update [WorldWideTorrents.eu] PM Enhancements.user.js

--- WorldWideTorrents/[WorldWideTorrents.eu] PM Enhancements.user.js
+++ WorldWideTorrents/[WorldWideTorrents.eu] PM Enhancements.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [WorldWideTorrents.eu] PM Enhancements
 // @namespace    PXgamer
-// @version      0.1
+// @version      0.2
 // @description  Adds a few changes to the mailbox screen.
 // @author       PXgamer
 // @match        *worldwidetorrents.eu/mailbox.php*
@@ -37,10 +37,27 @@
 		i++;
 	});
 
-	$('[id^="msg_"]').each(function(){
-		$(this).css('border-radius', '4px');
-		$(this).css('border', '1px solid #e3e3e3');
-		$(this).css('box-shadow', 'inset 0 1px 1px rgba(0,0,0,.05)');
-		$(this).css('padding', '9px');
+
+	$('#wrapper > div.myTable > div:nth-child(1) > div.main > table > tbody > tr > td:nth-child(1) > div.myFrame > div.wrapper').append('<div id="preview_box"></div>');
+
+	$('#preview_box').css('border-radius', '4px');
+	$('#preview_box').css('border', '1px solid #e3e3e3');
+	$('#preview_box').css('box-shadow', 'inset 0 1px 1px rgba(0,0,0,.05)');
+	$('#preview_box').css('padding', '9px');
+	$('#preview_box').css('background', '#ffffff');
+
+	$('[href^="javascript:read("]').on('click', function (e) {
+		var id = e.target.toString().split('read(')[1].split(')')[0];
+		var x = document.getElementById('msg_'+ id);
+		var y = document.getElementById('img_'+ id);
+		if (x.style.display === '') {
+			x.style.display='none';y.src='images/plus.gif';
+		}
+		else {
+			x.style.display='';y.src='images/minus.gif';
+		}
+
+		$('#preview_box').html(x);
+		$('#preview_box tr').css('display', 'block');
 	});
 })();
