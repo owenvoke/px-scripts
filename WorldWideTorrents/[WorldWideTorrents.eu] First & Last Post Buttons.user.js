6e14aa66dc1e42485eebb59f43fbc012972dc7c7
Owen Voke <owzie123@gmail.com>
2016-09-23 16:02:21 +0100

    Create [WorldWideTorrents.eu] First & Last Post Buttons.user.js

---
+++ WorldWideTorrents/[WorldWideTorrents.eu] First & Last Post Buttons.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         [WorldWideTorrents.eu] First & Last Post Links
+// @namespace    PXgamer
+// @version      0.1
+// @description  Adds links for first and last post to the forums.
+// @author       PXgamer
+// @include      https://worldwidetorrents.eu/forums.php?action=viewforum&forumid=*
+// @require      https://code.jquery.com/jquery-3.1.0.min.js
+// @grant        none
+// ==/UserScript==
+/*jshint multistr: true */
+
+(function() {
+    'use strict';
+
+    $('.alt1').each(function() {
+        var url = $(this).find('a').first().attr('href');
+        $(this).append('<p style="margin-bottom: 5px;"></p><a class="btnLink" href="/'+url+'">First Post</a> | <a class="btnLink" href="/'+url+'&page=last#last">Latest Post</a></td>');
+    });
+})();
